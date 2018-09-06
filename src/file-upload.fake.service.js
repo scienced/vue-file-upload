0cd1e8606cc3ba1532a80f0bd9a922b6cb3143e8
Scienced <michiel@mctol.nl>
2018-09-06 09:02:22 +0200

    Initial commit

---
+++ src/file-upload.fake.service.js
@@ -0,0 +1,74 @@
+import * as Papa from 'papaparse';
+import * as numeral from 'numeral';
+
+function upload(formData) {
+    const photos = formData.getAll('photos');
+    console.log(photos)
+
+    const promises = photos.map((x) => getImage(x)
+        .then(img => ({
+            id: img,
+            originalName: x.name,
+            fileName: x.name,
+            url: img
+        })));
+    return Promise.all(promises);
+}
+
+function getImage(file) {
+    return new Promise((resolve, reject) => {
+        const fReader = new FileReader();
+        const img = document.createElement('img');
+
+        fReader.readAsDataURL(file);
+
+        //file ingelezen
+        Papa.parse(file, { header: true,
+            complete: function(results) {
+                console.log("Finished:", results.data);
+                //cleanData(results.data)
+            }
+        });
+
+
+
+        fReader.onload = () => {
+            img.src = fReader.result;
+            resolve(getBase64Image(img));
+        }
+
+        
+    })
+}
+
+function getBase64Image(img) {
+    const canvas = document.createElement('canvas');
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(img, 0, 0);
+    const dataURL = img.src;
+    return dataURL;
+}
+
+function cleanData(data) {
+    const cleanedData = []
+    for (const row of data) 
+        //voor elke row in de array
+    {
+        const tempRow = ''
+        tempRow.tradeTimeStamp = new Date(row.Trade_Date)
+        tempRow.currency.concat(row.Buy_Cur,"-",row.Sell_Cur)
+        //tempRow.quantity = numeral(row.)
+        //tempRow.rate = 
+
+
+
+    }
+
+
+}
+
+export { upload }
+
